c3a59facb04c72f7116001daad35855b68b8e111
trajrealm <trajrealm>
2025-06-18 14:51:50 -0400

    test render 2

--- src/server/test_render.py
+++ src/server/test_render.py
@@ -8,3 +8,9 @@ print("sys.path:", sys.path)
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 print("Updated sys.path:", sys.path)
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
