195219bfe4d03ed9a2e5c65ea04b4d382399ff8f
trajrealm <trajrealm>
2025-06-18 15:12:19 -0400

    path inserted for render test2

--- src/server/routes.py
+++ src/server/routes.py
@@ -1,4 +1,5 @@
 import sys
+import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
