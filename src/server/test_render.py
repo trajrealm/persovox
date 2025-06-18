895b212687f0231fd5492e4c38e75083a5c4ec16
trajrealm <trajrealm>
2025-06-18 15:02:54 -0400

    test render  3

--- src/server/test_render.py
+++ src/server/test_render.py
@@ -5,7 +5,7 @@ print("Current Working Directory:", os.getcwd())
 print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
 print("sys.path:", sys.path)
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 print("Updated sys.path:", sys.path)
 
